93cf666465bdb9c680a48102b56f284e3c09eb10
Sam Selikoff <sam.selikoff@gmail.com>
2023-08-08 17:43:01 -0400

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,102 @@
 "use client";
 
-import { AnimatePresence, motion } from "framer-motion";
-import { CSSProperties, useState } from "react";
+import { Dialog } from "@headlessui/react";
+import { CheckIcon } from "@heroicons/react/24/outline";
+import { AnimatePresence, MotionConfig, motion } from "framer-motion";
+import { useState } from "react";
 
-export default function Home() {
-  let [open, setOpen] = useState(false);
+export default function Example() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="mt-48">
-      <button
-        onClick={() => setOpen(!open)}
-        className="px-3 py-2 bg-blue-500 text-white rounded"
-      >
-        Click
-      </button>
+    <div>
+      <div className="mt-8">
+        <button
+          className="inline-flex w-full justify-center rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 sm:ml-3 sm:w-auto"
+          onClick={() => setOpen(!open)}
+        >
+          Toggle
+        </button>
+      </div>
 
-      {/* <div className="mt-8 [--opacity-start:0%] [--opacity-end:100%] [--scale-start:0.8] [--scale-end:1]" style={{ */}
-      <div
-        className="mt-8
-          [--opacity-start:0%] [--opacity-end:100%]
-          md:[--opacity-start:100%] md:[--opacity-end:100%]
-          [--x-start:0px] [--x-end:0px]
-          md:[--x-start:100px] md:[--x-end:0px]
-        "
-        style={
-          {
-            "--scale-start": 1,
-            "--scale-start-string": "'1'",
-            "--scale-end": 0,
-            // "--x-start": "100px",
-            // "--x-end": "0px",
-          } as CSSProperties
-        }
+      <MotionConfig
+        transition={{ type: "spring", bounce: 0.3, duration: open ? 0.7 : 0.4 }}
       >
-        <AnimatePresence>
+        <AnimatePresence initial={false}>
           {open && (
-            <motion.div
-              initial={{
-                // opacity: "var(--opacity-start)",
-                // scale: "var(--scale-start)",
-                scale: "0%",
-                // x: "var(--x-start)",
-                // scale: 0,
-              }}
-              animate={{
-                // opacity: "var(--opacity-end)",
-                // scale: "var(--scale-end)",
-                scale: "100%",
-                // x: "var(--x-end)",
-                // scale: 1,
-              }}
-              exit={{
-                // opacity: "var(--opacity-start)",
-                // scale: "var(--scale-start)",
-                scale: "0%",
-                // x: "var(--x-start)",
-                // scale: 0,
-              }}
-              className="w-10 h-10 bg-green-500"
-            />
+            <Dialog
+              as={motion.div}
+              initial="closed"
+              animate="open"
+              exit="closed"
+              static
+              className="relative z-10"
+              open={open}
+              onClose={setOpen}
+            >
+              <motion.div
+                variants={{ closed: { opacity: 0 }, open: { opacity: 1 } }}
+                className="fixed inset-0 bg-gray-500 bg-opacity-75"
+              />
+
+              <div className="fixed inset-0 z-10 overflow-y-auto">
+                <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                  <Dialog.Panel
+                    as={motion.div}
+                    variants={{
+                      closed: {
+                        opacity: 0,
+                        scale: 0.9,
+                      },
+                      open: {
+                        opacity: 1,
+                        scale: 1,
+                      },
+                    }}
+                    className="
+                      relative overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl sm:my-8 sm:w-full sm:max-w-lg sm:p-6
+                    "
+                  >
+                    <div className="sm:flex sm:items-start">
+                      <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <CheckIcon
+                          className="h-6 w-6 text-green-600"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-base font-semibold leading-6 text-gray-900"
+                        >
+                          Subscription confirmed
+                        </Dialog.Title>
+                        <div className="mt-2">
+                          <p className="text-sm text-gray-500">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Fugiat deserunt ea voluptate vel aliquam!
+                            Quas, eum magnam nobis, necessitatibus quos natus
+                            labore quod.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                      <button
+                        type="button"
+                        className="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
+                        onClick={() => setOpen(false)}
+                      >
+                        Dismiss
+                      </button>
+                    </div>
+                  </Dialog.Panel>
+                </div>
+              </div>
+            </Dialog>
           )}
         </AnimatePresence>
-      </div>
+      </MotionConfig>
     </div>
   );
 }
